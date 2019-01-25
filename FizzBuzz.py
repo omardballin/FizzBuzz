93960885113204d7504f3f43dd1b3a0caf128783
Omar Diab <42656647+omardballin@users.noreply.github.com>
2019-01-25 16:08:51 -0600

    Add files via upload
    FizzBuzz Challenge

---
+++ FizzBuzz.py
@@ -0,0 +1,14 @@
+#FizzBuzz Challenge
+
+for number in range (0, 101):
+    if number == 0:
+        continue
+    elif number%15==0:
+        print('FizzBuzz')
+    elif number%3==0:
+        print('Fizz')
+    elif number%5==0:
+        print('Buzz')
+    else:
+        print(number)
+#
